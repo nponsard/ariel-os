cae766c3891b1ed44d47bf2353e573bc0ebab966
chrysn <chrysn@fsfe.org>
2024-11-28 16:36:48 +0100

    feat(coap): Share RIOT OS example modules in new coap-blinky test

---
+++ tests/coap-blinky/src/main.rs
@@ -0,0 +1,31 @@
+#![no_main]
+#![no_std]
+#![feature(impl_trait_in_assoc_type)]
+#![feature(used_with_arg)]
+
+#[path = "../../../examples/blinky/src/pins.rs"]
+mod pins;
+
+use ariel_os::gpio::Level;
+use ariel_os::gpio::Output;
+
+#[ariel_os::task(autostart, peripherals)]
+async fn coap_run(peripherals: pins::LedPeripherals) {
+    use coap_handler_implementations::{new_dispatcher, HandlerBuilder, ReportingHandlerBuilder};
+
+    let led = Output::new(peripherals.led, Level::Low);
+
+    // The micro:bit uses an LED matrix; pull the column line low; see blinky example
+    #[cfg(context = "microbit-v2")]
+    let _led_col1 = Output::new(peripherals.led_col1, Level::Low);
+
+    let handler = new_dispatcher()
+        // We offer a single resource: /led, which CBOR true or false can be PUT into
+        .at(
+            &["led"],
+            riot_coap_handler_demos::gpio::handler_for_output(led),
+        )
+        .with_wkc();
+
+    ariel_os::coap::coap_run(handler).await;
+}
