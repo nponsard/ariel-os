b324868c34472be37c59ea7f0d255600ba6bcb99
ROMemories <152802150+ROMemories@users.noreply.github.com>
2024-11-28 15:56:04 +0100

    chore(edition2024): mark `extern` blocks unsafe

--- src/ariel-os-storage/src/lib.rs
+++ src/ariel-os-storage/src/lib.rs
@@ -48,7 +48,7 @@ fn flash_range_from_linker() -> Range<u32> {
     #[cfg(not(context = "ariel-os"))]
     const OFFSET: usize = 0x0;
 
-    extern "C" {
+    unsafe extern "C" {
         static __storage_start: u32;
         static __storage_end: u32;
     }
