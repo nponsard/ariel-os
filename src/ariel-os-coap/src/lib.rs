65ffe32c971ef05d4b9a74be8f66dd1ff6990ba2
chrysn <chrysn@fsfe.org>
2025-01-15 13:42:36 +0100

    fix(coap): move demo credentials into inline module
    This makes them more visible, and isolates the imports done only for
    this purpose.

--- src/ariel-os-coap/src/lib.rs
+++ src/ariel-os-coap/src/lib.rs
@@ -25,6 +25,32 @@ static CLIENT: OnceLock<
     SendCell<embedded_nal_coap::CoAPRuntimeClient<'static, CONCURRENT_REQUESTS>>,
 > = OnceLock::new();
 
+mod demo_setup {
+    use cbor_macro::cbor;
+    use hexlit::hex;
+
+    /// Credential presented by any demo device.
+    pub(super) const DEVICE_CREDENTIAL: &[u8] = &hex!("A2026008A101A5010202410A2001215820BBC34960526EA4D32E940CAD2A234148DDC21791A12AFBCBAC93622046DD44F02258204519E257236B2A0CE2023F0931F1F386CA7AFDA64FCDE0108C224C51EABF6072");
+    /// Private key for `DEVICE_CREDENTIAL`.
+    pub(super) const DEVICE_KEY: [u8; 32] =
+        hex!("72cc4761dbd4c78f758931aa589d348d1ef874a7e303ede2f140dcf3e6aa4aac");
+
+    /// Scope usable by any client inside any demo device.
+    pub(super) const UNAUTHENTICATED_SCOPE: cboritem::CborItem =
+        cbor!([["/.well-known/core", 1], ["/poem", 1]]);
+
+    /// Scope usable by the the administrator of the demo device.
+    pub(super) const ADMIN_SCOPE: cboritem::CborItem = cbor!([
+            ["/stdout", 17 / GET and FETCH /],
+            ["/.well-known/core", 1],
+            ["/poem", 1]
+    ]);
+    /// Credential by which the administrator of any demo device is recognized.
+    ///
+    /// The corresponding private key is shipped in `tests/coap/client.cosekey`.
+    pub(super) const ADMIN_CREDENTIAL: &[u8] = &hex!("A2027734322D35302D33312D46462D45462D33372D33322D333908A101A5010202412B2001215820AC75E9ECE3E50BFC8ED60399889522405C47BF16DF96660A41298CB4307F7EB62258206E5DE611388A4B8A8211334AC7D37ECB52A387D257E6DB3C2A93DF21FF3AFFC8");
+}
+
 /// Runs a CoAP server with the given handler on the system's CoAP transports.
 ///
 /// As the CoAP stack gets ready, it also unblocks [`coap_client`].
@@ -60,23 +86,17 @@ pub async fn coap_run(handler: impl coap_handler::Handler + coap_handler::Report
         .await
         .unwrap();
 
-    use cbor_macro::cbor;
-    use hexlit::hex;
+    let own_key = demo_setup::DEVICE_KEY;
+    let own_credential = lakers::Credential::parse_ccs(demo_setup::DEVICE_CREDENTIAL)
+        .expect("Credential should be processable");
 
-    let own_key = hex!("72cc4761dbd4c78f758931aa589d348d1ef874a7e303ede2f140dcf3e6aa4aac");
-    let own_credential = lakers::Credential::parse_ccs(&hex!("A2026008A101A5010202410A2001215820BBC34960526EA4D32E940CAD2A234148DDC21791A12AFBCBAC93622046DD44F02258204519E257236B2A0CE2023F0931F1F386CA7AFDA64FCDE0108C224C51EABF6072")).expect("Credential should be processable");
-
-    let unauthenticated_scope: &[u8] = &cbor!([["/.well-known/core", 1], ["/poem", 1]]);
+    let unauthenticated_scope: &[u8] = &demo_setup::UNAUTHENTICATED_SCOPE;
     let unauthenticated_scope = coapcore::scope::AifValue::try_from(unauthenticated_scope)
         .expect("hard-coded scope fits this type")
         .into();
-    let admin_key = lakers::Credential::parse_ccs(&hex!("A2027734322D35302D33312D46462D45462D33372D33322D333908A101A5010202412B2001215820AC75E9ECE3E50BFC8ED60399889522405C47BF16DF96660A41298CB4307F7EB62258206E5DE611388A4B8A8211334AC7D37ECB52A387D257E6DB3C2A93DF21FF3AFFC8"))
-                            .expect("hard-coded credential fits this type");
-    let admin_scope: &[u8] = &cbor!([
-            ["/stdout", 17 / GET and FETCH /],
-            ["/.well-known/core", 1],
-            ["/poem", 1]
-    ]);
+    let admin_key = lakers::Credential::parse_ccs(demo_setup::ADMIN_CREDENTIAL)
+        .expect("hard-coded credential fits this type");
+    let admin_scope: &[u8] = &demo_setup::ADMIN_SCOPE;
     let admin_scope = coapcore::scope::AifValue::try_from(admin_scope)
         .expect("hard-coded scope fits this type")
         .into();
