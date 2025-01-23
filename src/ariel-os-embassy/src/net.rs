58e0887ba712213847158f61dddfde2456108f93
Kaspar Schleiser <kaspar@schleiser.de>
2025-01-23 13:42:26 +0100

    feat(build): introduce `network-config-dhcp`, `network-config-static`

--- src/ariel-os-embassy/src/net.rs
+++ src/ariel-os-embassy/src/net.rs
@@ -121,3 +121,33 @@ impl embassy_net::driver::RxToken for DummyDriver {
         match self.0 {}
     }
 }
+
+#[cfg(feature = "network-config-static")]
+#[no_mangle]
+fn __ariel_os_network_config() -> embassy_net::Config {
+    use ariel_os_utils::{ipv4_addr_from_env_or, u8_from_env_or};
+
+    let ipaddr = ipv4_addr_from_env_or!(
+        "CONFIG_NET_IPV4_STATIC_ADDRESS",
+        "10.42.0.61",
+        "static IPv4 address",
+    );
+
+    let gw_addr = ipv4_addr_from_env_or!(
+        "CONFIG_NET_IPV4_STATIC_GATEWAY_ADDRESS",
+        "10.42.0.1",
+        "static IPv4 gateway address",
+    );
+
+    let prefix_len = u8_from_env_or!(
+        "CONFIG_NET_IPV4_STATIC_CIDR_PREFIX_LEN",
+        24,
+        "static IPv4 CIDR prefix length"
+    );
+
+    embassy_net::Config::ipv4_static(embassy_net::StaticConfigV4 {
+        address: embassy_net::Ipv4Cidr::new(ipaddr, prefix_len),
+        dns_servers: heapless::Vec::new(),
+        gateway: Some(gw_addr),
+    })
+}
