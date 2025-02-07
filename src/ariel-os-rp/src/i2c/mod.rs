2d5577cbe8048be1c3b65788efa80410f69eea60
Kaspar Schleiser <kaspar@schleiser.de>
2025-02-07 16:18:05 +0100

    fix(rp235xa): enable i2c controller

--- src/ariel-os-rp/src/i2c/mod.rs
+++ src/ariel-os-rp/src/i2c/mod.rs
@@ -7,7 +7,7 @@ pub mod controller;
 pub fn init(peripherals: &mut crate::OptionalPeripherals) {
     // Take all I2C peripherals and do nothing with them.
     cfg_if::cfg_if! {
-        if #[cfg(context = "rp2040")] {
+        if #[cfg(context = "rp")] {
             let _ = peripherals.I2C0.take().unwrap();
             let _ = peripherals.I2C1.take().unwrap();
         } else {
