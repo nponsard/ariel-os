796a7f79dc97a2fb0d89a07729431acf65431a4e
ROMemories <152802150+ROMemories@users.noreply.github.com>
2025-03-18 17:28:23 +0100

    refactor(power)!: simplify the `power::reboot()` implementation
    BREAKING CHANGE: this removes the `power` Cargo feature

---
+++ examples/power/README.md
@@ -0,0 +1,12 @@
+# power
+
+## About
+
+This application demonstrates power management capabilities.
+Currently the application will print a log line and reboot every 3 seconds.
+
+## How to run
+
+In this directory, run
+
+    laze build -b nrf52840dk run
--- src/ariel-os-power/src/lib.rs
+++ src/ariel-os-power/src/lib.rs
@@ -1,61 +1,27 @@
 //! Provides power management functionality.
+
 #![deny(clippy::pedantic)]
 #![deny(missing_docs)]
 #![no_std]
 
-#[cfg(context = "cortex-m")]
-use cortex_m::peripheral::SCB;
-
-#[cfg(any(
-    context = "esp32",
-    context = "esp32c3",
-    context = "esp32c6",
-    context = "esp32s3"
-))]
-use esp_hal::reset::software_reset;
-
-/// Reboot the MCU
-/// This function initiates a software reset of the microcontroller.
+/// Reboots the MCU.
 ///
-/// # Note
-///
-/// This function never returns as it triggers an immediate system reset.
-///
-/// # Panics
-///
-/// … if called on an unsupported platform.
+/// This function initiates a software reset of the microcontroller and never returns.
 pub fn reboot() -> ! {
-    #[cfg(context = "cortex-m")]
-    {
-        SCB::sys_reset()
-    }
-    #[cfg(any(
-        context = "esp32",
-        context = "esp32c3",
-        context = "esp32c6",
-        context = "esp32s3"
-    ))]
-    {
-        software_reset()
-    }
-
-    #[cfg(all(
-        not(any(
-            context = "cortex-m",
-            context = "esp32",
-            context = "esp32c3",
-            context = "esp32c6",
-            context = "esp32s3"
-        )),
-        not(feature = "doc"),
-        not(feature = "no-boards")
-    ))]
-    {
-        compile_error!("reboot is not yet implemented for this platform")
-    }
-
-    #[allow(unreachable_code, reason = "fallback loop")]
-    loop {
-        core::hint::spin_loop();
+    cfg_if::cfg_if! {
+        if #[cfg(context = "cortex-m")] {
+            cortex_m::peripheral::SCB::sys_reset()
+        } else if #[cfg(context = "esp")] {
+            // TODO: when updating `esp_hal`, use `system::software_reset()` instead, remove the
+            // semicolon and the empty loop below as `system::software_reset()` never returns.
+            esp_hal::reset::software_reset();
+            #[expect(clippy::empty_loop, reason = "software_reset() should eventually never return")]
+            loop {}
+        } else if #[cfg(context = "ariel-os")] {
+            compile_error!("reboot is not yet implemented for this platform")
+        } else {
+            #[expect(clippy::empty_loop, reason = "for platform-independent tooling only")]
+            loop {}
+        }
     }
 }
