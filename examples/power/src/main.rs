ab61649e5c5621d1f6e8525cd23452af05d296b7
Ollrogge <nils-ollrogge@outlook.de>
2025-03-18 09:47:41 +0100

    feat(examples): add pwr-mgmt example application

---
+++ examples/power/src/main.rs
@@ -0,0 +1,18 @@
+#![no_main]
+#![no_std]
+#![feature(impl_trait_in_assoc_type)]
+#![feature(used_with_arg)]
+
+use ariel_os::{
+    debug::log::*,
+    power,
+    time::{Duration, Timer},
+};
+
+#[ariel_os::task(autostart)]
+async fn main() {
+    info!("Rebooting in 3 sec");
+
+    Timer::after(Duration::from_secs(3)).await;
+    power::reboot();
+}
