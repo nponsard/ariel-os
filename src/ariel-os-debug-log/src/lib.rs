01a825573c9e401e25c012c6f9e4fd49529c478a
chrysn <chrysn@fsfe.org>
2025-04-07 11:27:18 +0200

    feat(log): add bytes and cbor helpers for logging

--- src/ariel-os-debug-log/src/lib.rs
+++ src/ariel-os-debug-log/src/lib.rs
@@ -172,3 +172,52 @@ mod log_macros {
         ($($arg:tt)*) => {};
     }
 }
+
+/// A newtype around byte slices used for all of Ariel OS's logging facades that turns the bytes
+/// into some hex output.
+///
+/// Its preferred output `00 11 22 33`, but log facades may also produce something like `[00, 11,
+/// 22, 33]` (eg. while that is cheaper on defmt).
+///
+/// Instead of writing some variation of `info!("Found bytes {:02x}", data)`, you can write
+/// `info!("Found bytes {}", Hex(data))`.
+pub struct Hex<T: AsRef<[u8]>>(pub T);
+
+impl<T: AsRef<[u8]>> core::fmt::Display for Hex<T> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "{:02x?}", self.0.as_ref())
+    }
+}
+
+#[cfg(feature = "defmt")]
+impl<T: AsRef<[u8]>> defmt::Format for Hex<T> {
+    fn format(&self, f: defmt::Formatter) {
+        ::defmt::write!(f, "{=[u8]:02x}", self.0.as_ref())
+    }
+}
+
+/// A newtype around byte slices used for all of Ariel OS's logging facades that prefers
+/// interpreting the data as CBOR.
+///
+/// Its preferred outptu is CBOR Diagnostic Notation (EDN), but showing hex is also acceptable.
+///
+/// Instead of writing some variation of `info!("Found bytes {:cbor}", item)`, you can write
+/// `info!("Found bytes {}", Cbor(item))`.
+///
+/// Note that using this wrapper is not necessary when using a
+/// [`cboritem::CborItem`](https://docs.rs/cboritem/latest/cboritem/struct.CborItem.html) as it
+/// already does something similar on its own.
+pub struct Cbor<T: AsRef<[u8]>>(pub T);
+
+impl<T: AsRef<[u8]>> core::fmt::Display for Cbor<T> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        Hex(self.0.as_ref()).fmt(f)
+    }
+}
+
+#[cfg(feature = "defmt")]
+impl<T: AsRef<[u8]>> defmt::Format for Cbor<T> {
+    fn format(&self, f: defmt::Formatter) {
+        ::defmt::write!(f, "{=[u8]:cbor}", self.0.as_ref())
+    }
+}
