abd21bebaecd61077f00c27e79ee9ffe51d7e92e
Kaspar Schleiser <kaspar@schleiser.de>
2025-05-22 13:13:56 +0200

    chore: conflict `ram-tiny` for examples / tests that need it

--- examples/threading/src/main.rs
+++ examples/threading/src/main.rs
@@ -9,7 +9,7 @@ fn thread0() {
 }
 
 // `stacksize` and `priority` can be arbitrary expressions.
-#[ariel_os::thread(autostart, stacksize = 2048, priority = 2)]
+#[ariel_os::thread(autostart, stacksize = 512, priority = 2)]
 fn thread1() {
     info!("Hello from thread 1");
 }
