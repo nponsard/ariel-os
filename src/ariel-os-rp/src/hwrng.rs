86450faedd64876df65991065775f689f636fe6f
ROMemories <152802150+ROMemories@users.noreply.github.com>
2025-06-04 10:08:50 +0200

    fix(rp235x): use the built-in TRNG as HWRNG instead of `RoscRng`
    Signed-off-by: ROMemories <152802150+ROMemories@users.noreply.github.com>

--- src/ariel-os-rp/src/hwrng.rs
+++ src/ariel-os-rp/src/hwrng.rs
@@ -1,3 +1,29 @@
-pub fn construct_rng(_peripherals: &mut crate::OptionalPeripherals) {
-    ariel_os_random::construct_rng(embassy_rp::clocks::RoscRng);
+pub fn construct_rng(peripherals: &mut crate::OptionalPeripherals) {
+    #[cfg(context = "rp2040")]
+    let hwrng = {
+        let _ = peripherals; // Mark used
+
+        // NOTE(datasheet): The RP2040 RNG "does not meet the requirements of randomness for
+        // security systems because it can be compromised".
+        embassy_rp::clocks::RoscRng
+    };
+
+    #[cfg(context = "rp235xa")]
+    let hwrng = {
+        embassy_rp::bind_interrupts!(struct Irqs {
+            TRNG_IRQ => embassy_rp::trng::InterruptHandler<embassy_rp::peripherals::TRNG>;
+        });
+
+        let trng = peripherals.TRNG.take().unwrap();
+
+        // Enable all entropy checks to ensure acceptable results.
+        let mut config = embassy_rp::trng::Config::default();
+        config.disable_autocorrelation_test = false;
+        config.disable_crngt_test = false;
+        config.disable_von_neumann_balancer = false;
+
+        embassy_rp::trng::Trng::new(trng, Irqs, config)
+    };
+
+    ariel_os_random::construct_rng(hwrng);
 }
