b23855dc7bbf66931756fcac95667544a2555f69
Nils Ponsard <nils.ponsard@inria.fr>
2025-06-13 12:34:21 +0200

    proposal: use a common function to initialize host resources

--- src/ariel-os-embassy-common/src/ble.rs
+++ src/ariel-os-embassy-common/src/ble.rs
@@ -1,5 +1,24 @@
 //! Common BLE types to be used across different HALs.
 
+use static_cell::StaticCell;
+use trouble_host::HostResources;
+
+// Safe value of 27, compatible with all versions.
+pub const L2CAP_MTU: usize = 27;
+
+// Safe defaults used in trouble_host examples
+pub const MAX_CONNS: usize = 1;
+pub const MAX_CHANNELS: usize = 1;
+
+pub type BleHostResource = HostResources<MAX_CONNS, MAX_CHANNELS, L2CAP_MTU>;
+
+static HOST_RESOURCES: StaticCell<BleHostResource> = StaticCell::new();
+
+/// Initialize the BLE host resources.
+pub fn get_ble_host_ressources() -> &'static mut BleHostResource {
+    HOST_RESOURCES.init(HostResources::new())
+}
+
 /// Configuration for the BLE stack.
 ///
 /// You can customize it using the `ble-config-override` feature.
