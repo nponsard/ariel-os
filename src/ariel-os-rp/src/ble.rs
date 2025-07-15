485eed1e01fc2b51a691ad16a0f8ce2b115ceb2c
jkr-inria <210673275+jkr-ariel@users.noreply.github.com>
2025-07-15 14:51:08 +0200

    feat(laze): introduce new `ble-cyw43` module
    Signed-off-by: jkr-inria <210673275+jkr-ariel@users.noreply.github.com>

--- src/ariel-os-rp/src/ble.rs
+++ src/ariel-os-rp/src/ble.rs
@@ -2,7 +2,7 @@ use bt_hci::controller::ExternalController;
 use cyw43::bluetooth::BtDriver;
 use embassy_sync::once_lock::OnceLock;
 
-/// Number of command slots for the Bluetooth driver
+/// Number of command slots for the Bluetooth driver.
 pub const SLOTS: usize = 10;
 
 pub(crate) static STACK: OnceLock<
