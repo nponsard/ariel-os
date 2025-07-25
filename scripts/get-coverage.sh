4ae2a2d51faeae64d17197306f07445cb0820d15
Nils Ponsard <nils.ponsard@inria.fr>
2025-07-25 12:13:21 +0200

    wip: add ci

--- scripts/get-coverage.sh
+++ scripts/get-coverage.sh
@@ -9,11 +9,20 @@ laze build -c -s nightly --builders host  --global -m --keep-going=0 -s coverage
 
 for file in  target/debug/doctestbins/*/rust_out ;
 do 
-  [[ -x $file ]] && printf "%s %s " -object $file >> build/profile/objects;
-done 
+  if [ -f "$file" ]; then
+    printf "%s %s " -object $file >> build/profile/objects;
+  fi
+done
+
+RUSTUP_TOOLCHAIN=$(scripts/rust-toolchain.sh)
+sysroot=$(rustc --print sysroot)
+
+profdata=$(find $sysroot -name "llvm-profdata")
+cov=$(find $sysroot -name "llvm-cov")
+
 
 objects=$(cat build/profile/objects)
 
-llvm-profdata merge -sparse build/profile/*.profraw -o build/profile/output.profdata  
+${profdata} merge -sparse build/profile/*.profraw -o build/profile/output.profdata  
 
-llvm-cov export ${objects} -Xdemangler=rustfilt -instr-profile=build/profile/output.profdata -format=lcov --ignore-filename-regex='/.cargo' --ignore-filename-regex='rustc/' --ignore-filename-regex='/.rustup' > build/profile/coverage.txt
+${cov} export ${objects} -Xdemangler=rustfilt -instr-profile=build/profile/output.profdata -format=lcov --ignore-filename-regex='/.cargo' --ignore-filename-regex='rustc/' --ignore-filename-regex='/.rustup' > build/profile/coverage.txt
