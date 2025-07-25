071e180a5d71dc51112db347fe4009ae0b2cdf1d
ROMemories <152802150+ROMemories@users.noreply.github.com>
2025-07-25 14:43:04 +0200

    docs(examples): add an example demonstrating homogeneous sensor access
    Signed-off-by: ROMemories <152802150+ROMemories@users.noreply.github.com>

---
+++ examples/sensors-debug/README.md
@@ -0,0 +1,11 @@
+# sensors-debug
+
+## About
+
+This application is demonstrating the sensor API usage in Ariel OS.
+
+## How to run
+
+In this directory, run
+
+    laze build -b nrf52840dk run
---
+++ examples/sensors-debug/src/sensors.rs
@@ -0,0 +1,4 @@
+//! This module is intended to contain the auto-@generated instantiation and registration of sensor
+//! drivers.
+
+// This example does not currently register any sensor drivers, they will be added later.
