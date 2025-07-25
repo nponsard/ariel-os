27e6f601c1c5edf1d70d60d1cd583277f529a9fc
ROMemories <152802150+ROMemories@users.noreply.github.com>
2025-07-25 14:45:38 +0200

    refactor(sensors): move the measurement unit custom formatting to a macro
    Signed-off-by: ROMemories <152802150+ROMemories@users.noreply.github.com>

--- src/ariel-os-sensors/src/measurement_unit.rs
+++ src/ariel-os-sensors/src/measurement_unit.rs
@@ -82,44 +82,50 @@ pub enum MeasurementUnit {
     Weber,
 }
 
+macro_rules! provide_unit_fmt {
+    ($unit:expr, $f:expr) => {
+        match $unit {
+            Self::AccelG => write!($f, "g"),
+            Self::Ampere => write!($f, "A"),
+            Self::Becquerel => write!($f, "Bq"),
+            Self::Bool => write!($f, ""),
+            Self::Candela => write!($f, "cd"),
+            // As recommended by the Unicode Standard v16 (U+00B0 + U+0043)
+            Self::Celsius => write!($f, "°C"),
+            Self::Coulomb => write!($f, "C"),
+            Self::Decibel => write!($f, "dB"),
+            Self::Farad => write!($f, "F"),
+            Self::Gram => write!($f, "g"),
+            Self::Gray => write!($f, "Gy"),
+            Self::Henry => write!($f, "H"),
+            Self::Hertz => write!($f, "Hz"),
+            Self::Joule => write!($f, "J"),
+            Self::Katal => write!($f, "kat"),
+            Self::Kelvin => write!($f, "K"),
+            Self::Lumen => write!($f, "lm"),
+            Self::Lux => write!($f, "lx"),
+            Self::Meter => write!($f, "m"),
+            Self::Mole => write!($f, "mol"),
+            Self::Newton => write!($f, "N"),
+            Self::Ohm => write!($f, "Ω"),
+            Self::Pascal => write!($f, "Pa"),
+            Self::Percent => write!($f, "%"),
+            Self::PercentageRelativeHumidity => write!($f, "%RH"),
+            Self::Radian => write!($f, "rad"),
+            Self::Second => write!($f, "s"),
+            Self::Siemens => write!($f, "S"),
+            Self::Sievert => write!($f, "Sv"),
+            Self::Steradian => write!($f, "sr"),
+            Self::Tesla => write!($f, "T"),
+            Self::Volt => write!($f, "V"),
+            Self::Watt => write!($f, "W"),
+            Self::Weber => write!($f, "Wb"),
+        }
+    };
+}
+
 impl core::fmt::Display for MeasurementUnit {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        #[expect(clippy::match_same_arms)]
-        match self {
-            Self::AccelG => write!(f, "g"),
-            Self::Ampere => write!(f, "A"),
-            Self::Becquerel => write!(f, "Bq"),
-            Self::Bool => write!(f, ""),
-            Self::Candela => write!(f, "cd"),
-            Self::Celsius => write!(f, "°C"), // The Unicode Standard v15 recommends using U+00B0 + U+0043.
-            Self::Coulomb => write!(f, "C"),
-            Self::Decibel => write!(f, "dB"),
-            Self::Farad => write!(f, "F"),
-            Self::Gram => write!(f, "g"),
-            Self::Gray => write!(f, "Gy"),
-            Self::Henry => write!(f, "H"),
-            Self::Hertz => write!(f, "Hz"),
-            Self::Joule => write!(f, "J"),
-            Self::Katal => write!(f, "kat"),
-            Self::Kelvin => write!(f, "K"),
-            Self::Lumen => write!(f, "lm"),
-            Self::Lux => write!(f, "lx"),
-            Self::Meter => write!(f, "m"),
-            Self::Mole => write!(f, "mol"),
-            Self::Newton => write!(f, "N"),
-            Self::Ohm => write!(f, "Ω"),
-            Self::Pascal => write!(f, "Pa"),
-            Self::Percent => write!(f, "%"),
-            Self::PercentageRelativeHumidity => write!(f, "%RH"),
-            Self::Radian => write!(f, "rad"),
-            Self::Second => write!(f, "s"),
-            Self::Siemens => write!(f, "S"),
-            Self::Sievert => write!(f, "Sv"),
-            Self::Steradian => write!(f, "sr"),
-            Self::Tesla => write!(f, "T"),
-            Self::Volt => write!(f, "V"),
-            Self::Watt => write!(f, "W"),
-            Self::Weber => write!(f, "Wb"),
-        }
+        provide_unit_fmt!(self, f)
     }
 }
