5c73e5448360aee7f12b9f7978dac15a855a066b
Nils Ponsard <nils.ponsard@inria.fr>
2025-08-27 17:14:35 +0200

    fix: reduce size of fixed types

--- src/ariel-os-embassy-common/src/gnss.rs
+++ src/ariel-os-embassy-common/src/gnss.rs
@@ -67,22 +67,233 @@ impl core::fmt::Display for Config {
     }
 }
 
+pub trait FixedPoint32 {
+    const FRAC_BITS: u8;
+    const SIGNED: bool;
+
+    fn from_bits(bits: u32) -> Self;
+
+    fn from_f32(value: f32) -> Self
+    where
+        Self: Sized,
+    {
+        let scale = 1 << Self::FRAC_BITS;
+        if Self::SIGNED {
+            let scaled = (value * scale as f32).round() as i32;
+            Self::from_bits(scaled as u32)
+        } else {
+            let scaled = (value * scale as f32).round() as u32;
+            Self::from_bits(scaled)
+        }
+    }
+
+    // Should be fine use `as u32` on signed types as well, as we only use the raw bits
+    fn as_bits(&self) -> u32;
+
+    fn as_be_bytes(&self) -> [u8; 4] {
+        self.as_bits().to_be_bytes()
+    }
+
+    /// Possibly lossy conversion to f32
+    fn as_f32(&self) -> f32 {
+        let scale = 1 << Self::FRAC_BITS;
+        if Self::SIGNED {
+            (self.as_bits() as i32) as f32 / scale as f32
+        } else {
+            self.as_bits() as f32 / scale as f32
+        }
+    }
+
+    fn display_format(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        let integer_part = self.as_bits() >> Self::FRAC_BITS;
+        let mut fractional_part = self.as_bits() & ((1 << Self::FRAC_BITS) - 1);
+        if Self::SIGNED {
+            write!(f, "{}", integer_part as i32)?;
+        } else {
+            write!(f, "{}", integer_part)?;
+        }
+
+        if fractional_part == 0 {
+            return Ok(());
+        }
+        write!(f, ".")?;
+
+        while fractional_part > 0 {
+            // Multiply by 10 to get the next decimal digit
+            fractional_part *= 10;
+            let digit = fractional_part >> Self::FRAC_BITS;
+            write!(f, "{}", digit)?;
+            // Remove the integer part
+            fractional_part &= (1 << Self::FRAC_BITS) - 1;
+        }
+        Ok(())
+    }
+}
+
+pub trait FixedPoint16 {
+    const FRAC_BITS: u8;
+    const SIGNED: bool;
+
+    fn from_bits(bits: u16) -> Self;
+
+    fn from_f32(value: f32) -> Self {
+        let scale = 1 << Self::FRAC_BITS;
+        if Self::SIGNED {
+            let scaled = (value * scale as f32).round() as i32;
+            Self::from_bits(scaled as u32)
+        } else {
+            let scaled = (value * scale as f32).round() as u32;
+            Self::from_bits(scaled)
+        }
+    }
+
+    // Should be fine use `as u16` on signed types as well, as we only use the raw bits
+    fn as_bits(&self) -> u16;
+
+    fn as_be_bytes(&self) -> [u8; 2] {
+        self.as_bits().to_be_bytes()
+    }
+
+    /// Possibly lossy conversion to f32
+    fn as_f32(&self) -> f32 {
+        let scale = 1 << Self::FRAC_BITS;
+        if Self::SIGNED {
+            (self.as_bits() as i16) as f32 / scale as f32
+        } else {
+            self.as_bits() as f32 / scale as f32
+        }
+    }
+
+    fn display_format(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        let integer_part = self.as_bits() >> Self::FRAC_BITS;
+        let mut fractional_part = self.as_bits() & ((1 << Self::FRAC_BITS) - 1);
+        if Self::SIGNED {
+            write!(f, "{}", integer_part as i32)?;
+        } else {
+            write!(f, "{}", integer_part)?;
+        }
+
+        if fractional_part == 0 {
+            return Ok(());
+        }
+        write!(f, ".")?;
+
+        while fractional_part > 0 {
+            // Multiply by 10 to get the next decimal digit
+            fractional_part *= 10;
+            let digit = fractional_part >> Self::FRAC_BITS;
+            write!(f, "{}", digit)?;
+            // Remove the integer part
+            fractional_part &= (1 << Self::FRAC_BITS) - 1;
+        }
+        Ok(())
+    }
+}
+
+/// Longitude in decimal degrees.
+/// Stored in fixed-point format.
+/// To initialize this struct, you need to provide a fixed-point representation of the value.
+/// This fixed-point representation should have 23 fractional bits, be signed and stored in an i32.
+/// 1 sign bit, 8 integer bits, 23 fractional bits, this makes the difference between two consecutive values at the equator of 40,075,016.7/(360*2^23) ~= 0.013 meters
+/// Range from -180 to 180 degrees.
+///
+/// ## Conversion example
+///     let origin = -122.41943456;
+///     let fixed_origin = FixedI32::<U23>::from_num(origin);
+///     let manually_fixed: i32 = (origin * (1 << 23) as f64).round() as i32;
+///
+///     let lon = Longitude {
+///         inner: fixed_origin.to_bits(),
+///     };
+///     let lon2 = Longitude {
+///         inner: manually_fixed,
+///     };
+///
+pub struct Longitude {
+    inner: i32,
+}
+impl FixedPoint32 for Longitude {
+    const FRAC_BITS: u8 = 23;
+    const SIGNED: bool = true;
+
+    fn from_bits(bits: u32) -> Self {
+        Self { inner: bits as i32 }
+    }
+    fn as_bits(&self) -> u32 {
+        self.inner as u32
+    }
+}
+
+impl core::fmt::Display for Longitude {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        self.display_format(f)
+    }
+}
+
+/// Latitude in decimal degrees
+/// Stored in fixed-point format.
+/// To initialize this struct, you need to provide a fixed-point representation of the value.
+/// This fixed-point representation should have 23 fractional bits, be signed and stored in an i32.
+/// 1 sign bit, 8 integer bits, 23 fractional bits, this makes the difference between two consecutive values at the equator of 40,075,016.7/(360*2^23) ~= 0.013 meters
+/// Range from -90 to 90 degrees.
+pub struct Latitude {
+    inner: i32,
+}
+
+impl FixedPoint32 for Latitude {
+    const FRAC_BITS: u32 = 23;
+    const SIGNED: bool = true;
+
+    fn as_bits(&self) -> u32 {
+        self.inner as u32
+    }
+    fn from_bits(bits: u32) -> Self {
+        Self { inner: bits as i32 }
+    }
+}
+
+/// Altitude in meters above sea level.
+/// Stored in fixed-point format.
+/// To initialize this struct, you need to provide a fixed-point representation of the value.
+/// This fixed-point representation should have 8 fractional bits, be signed and stored in an i32.
+/// 1 sign bit, 23 integer bits, 8 fractional bits, this gives a maximum altitude of ~8388 km. The difference between two consecutive values is 1/2^8 ~= 0.004 meters
+pub struct Altitude {
+    inner: i32,
+}
+impl FixedPoint32 for Altitude {
+    const FRAC_BITS: u32 = 8;
+    const SIGNED: bool = true;
+
+    fn as_bits(&self) -> u32 {
+        self.inner as u32
+    }
+    fn from_bits(bits: u32) -> Self {
+        Self { inner: bits as i32 }
+    }
+}
+
 /// Represents position data from GNSS.
 #[derive(Debug, Copy, Clone)]
 // #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub struct GnssPosition {
-    /// Latitude in degrees.
-    /// Positive values indicate north, negative values indicate south.
-    pub latitude: FixedI32<U24>, // 1 sign bit, 7 integer bits, 24 fractional bits
-    /// Longitude in degrees
+    /// Longitude in decimal degrees
     /// Positive values indicate east, negative values indicate west.
-    pub longitude: FixedI32<U23>, // 1 sign bit, 8 integer bits, 23 fractional bits, this makes the difference between two consecutive values at the equator of 40,075,016.7/(360*2^23) ~= 0.013 meters
+    /// From -180 to 180
+    pub longitude: Longitude,
+    /// Latitude in decimal degrees.
+    /// Positive values indicate north, negative values indicate south.
+    /// From -90 to 90
+    pub latitude: Latitude,
     /// Altitude in meters above sea level
-    pub altitude: FixedI32<U6>, // 1 sign bit, 25 integer bits, 6 fractional bits, this gives a maximum altitude of 33554 km, GPS statellites orbit at about 20,200 km. The difference between two consecutive values is 1/2^6 ~= 0.015625 meters
-    /// Accuracy of the position in meters
-    pub accuracy: FixedU32<U8>, // 24 integer bits, 8 fractional bits, range from around 0 to 16777215 meters, with a difference between two consecutive values of 1/2^8 ~= 0.0039 meters
-    /// Altitude accuracy in meters
-    pub altitude_accuracy: FixedU32<U8>,
+    pub altitude: Altitude, // 1 sign bit, 23 integer bits, 8 fractional bits, this gives a maximum altitude of ~8388 km. The difference between two consecutive values is 1/2^8 ~= 0.004 meters
+    /// Horizontal accuracy in meters, the calculation of this value depends on the GNSS module.
+    /// An estimation from the HDOP can be done by multiplying the HDOP by the announced horizontal accuracy of the GNSS module.
+    /// Value range between 0 and 255 meters.
+    pub accuracy: FixedU16<U8>, // 8 integer bits, 8 fractional bits, range from around 0 to 255 meters, with a difference between two consecutive values of 1/2^8 ~= 0.0039 meters
+    /// Altitude accuracy in meters, the calculation of this value depends on the GNSS module.
+    /// An estimation from the VDOP can be done by multiplying the VDOP by the announced vertical accuracy of the GNSS module.
+    /// Value range between 0 and 255 meters.
+    pub altitude_accuracy: FixedU16<U8>,
 }
 
 #[cfg(feature = "defmt")]
@@ -110,17 +321,17 @@ impl core::fmt::Display for GnssPosition {
 #[derive(Debug, Copy, Clone)]
 pub struct GnssVelocity {
     /// Speed in m/s
-    pub speed: FixedU32<U20>, // 12 integer bits, 20 fractional bits. Makes max speed of around 4095 m/s, 10 time the speed of sound. Difference between two consecutive values is 1/2^20 ~= 9.54e-7 m/s
+    pub speed: FixedU16<U6>, // 10 integer bits, 6 fractional bits. Makes max speed of around 1023 m/s. Difference between two consecutive values is 1/2^6 ~= 0.0016 m/s or 0.057 km/h
     /// Speed accuracy in m/s
-    pub speed_accuracy: FixedU32<U20>,
+    pub speed_accuracy: FixedU16<U6>,
     /// Vertical speed in m/s
-    pub vertical_speed: FixedI32<U19>, // Vertical speed can be negative, losing some decimal precision (1.91e-6 m/s).
+    pub vertical_speed: FixedI16<U6>, // Vertical speed can be negative
     /// Vertical speed accuracy in m/s
-    pub vertical_speed_accuracy: FixedU32<U19>,
+    pub vertical_speed_accuracy: FixedU16<U6>,
     /// Heading in degrees (0–360)
-    pub heading: FixedU32<U23>, // 9 integer bits, 24 fractional bits. Difference between two consecutive values is 1/2^23 ~= 1.19e-7 degrees
+    pub heading: FixedU16<U7>, // 9 integer bits, 7 fractional bits. Difference between two consecutive values is 1/2^7 ~= 0.008 degrees
     /// Heading accuracy in degrees
-    pub heading_accuracy: FixedU32<U23>,
+    pub heading_accuracy: FixedU16<U7>,
 }
 
 #[cfg(feature = "defmt")]
@@ -146,7 +357,7 @@ impl core::fmt::Display for GnssVelocity {
 }
 
 // Based on NMEA RMC message
-/// Represents date and time information from GNSS.
+/// Represents date and time information from GNSS. All fields are in UTC.
 #[derive(Debug, Copy, Clone)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub struct GnssDateTime {
@@ -184,8 +395,6 @@ pub struct GnssData {
     pub velocity: Option<GnssVelocity>,
     /// The date and time information, if available.
     pub datetime: Option<GnssDateTime>,
-    /// The time when the data was recorded (from start of the MCU).
-    pub recorded_at: embassy_time::Instant,
 }
 
 impl core::fmt::Display for GnssData {
