bfb700ce1aa1bda6c3ec9fd4612de6275ad81766
ROMemories <152802150+ROMemories@users.noreply.github.com>
2025-08-28 15:54:20 +0200

    refactor(env-utils): use `const` `from_str_radix` where possible
    Signed-off-by: ROMemories <152802150+ROMemories@users.noreply.github.com>

--- src/ariel-os-utils/src/env.rs
+++ src/ariel-os-utils/src/env.rs
@@ -1,7 +1,7 @@
 pub use {const_panic, konst};
 
 macro_rules! define_env_with_default_macro {
-    ($macro_name:ident, $parse_fn_name:ident, $output_type_name:literal) => {
+    ($macro_name:ident, $output_type:ident, $output_type_name:literal) => {
         #[macro_export]
         macro_rules! $macro_name {
             // $doc is currently unused
@@ -10,8 +10,7 @@ macro_rules! define_env_with_default_macro {
             ($env_var:literal, $default:expr, $doc:literal) => {
                 const {
                     if let Some(str_value) = option_env!($env_var) {
-                        if let Ok(value) = $crate::env::konst::primitive::$parse_fn_name(str_value)
-                        {
+                        if let Ok(value) = $output_type::from_str_radix(str_value, 10) {
                             value
                         } else {
                             $crate::env::const_panic::concat_panic!(
@@ -32,8 +31,8 @@ macro_rules! define_env_with_default_macro {
     };
 }
 
-define_env_with_default_macro!(usize_from_env_or, parse_usize, "a usize");
-define_env_with_default_macro!(u8_from_env_or, parse_u8, "a u8");
+define_env_with_default_macro!(usize_from_env_or, usize, "a usize");
+define_env_with_default_macro!(u8_from_env_or, u8, "a u8");
 
 #[macro_export]
 macro_rules! bool_from_env_or {
