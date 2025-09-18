c2d1e8ecaa2148748326a445c50ecd25a7d95cfc
Koen Zandberg <koen@bergzand.net>
2025-09-18 14:47:39 +0200

    test(uart-loopback): add raspberry pi platform
    Signed-off-by: Koen Zandberg <koen@bergzand.net>

--- tests/uart-loopback/src/pins.rs
+++ tests/uart-loopback/src/pins.rs
@@ -1,1 +1,9 @@
 use ariel_os::hal::{peripherals, uart};
+
+#[cfg(context = "rp")]
+pub type TestUart<'a> = uart::UART0<'a>;
+#[cfg(context = "rp")]
+ariel_os::hal::define_peripherals!(Peripherals {
+    uart_rx: PIN_17,
+    uart_tx: PIN_16,
+});
