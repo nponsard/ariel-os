04219360fd1958d8612bdb7a9ff94afd30205179
Kaspar Schleiser <kaspar@schleiser.de>
2025-10-09 14:17:37 +0200

    chore(boards): drop led matrix quirks for bbc-microbit-vX
    Signed-off-by: Kaspar Schleiser <kaspar@schleiser.de>

--- src/ariel-os-boards/src/bbc-microbit-v1.rs
+++ src/ariel-os-boards/src/bbc-microbit-v1.rs
@@ -3,9 +3,6 @@
 pub mod pins {
 use ariel_os_hal::hal::peripherals;
 
-ariel_os_hal::define_peripherals!(LedPeripherals {
-led0: P0_13,
-});
 ariel_os_hal::define_peripherals!(ButtonPeripherals {
 button0: P0_17,
 button1: P0_26,
@@ -14,10 +11,4 @@ button1: P0_26,
 
 #[allow(unused_variables)]
 pub fn init(peripherals: &mut ariel_os_hal::hal::OptionalPeripherals) {
-{
-    // Set the LED matrix column for led0 to low
-    let pin = peripherals.P0_04.take().unwrap();
-    let output = ariel_os_hal::gpio::Output::new(pin, ariel_os_embassy_common::gpio::Level::Low);
-    core::mem::forget(output);
-}
 }
--- src/ariel-os-boards/src/bbc-microbit-v2.rs
+++ src/ariel-os-boards/src/bbc-microbit-v2.rs
@@ -3,9 +3,6 @@
 pub mod pins {
 use ariel_os_hal::hal::peripherals;
 
-ariel_os_hal::define_peripherals!(LedPeripherals {
-led0: P0_21,
-});
 ariel_os_hal::define_peripherals!(ButtonPeripherals {
 button0: P0_14,
 button1: P0_23,
@@ -14,10 +11,4 @@ button1: P0_23,
 
 #[allow(unused_variables)]
 pub fn init(peripherals: &mut ariel_os_hal::hal::OptionalPeripherals) {
-{
-    // Set the LED matrix column for led0 to low
-    let pin = peripherals.P0_28.take().unwrap();
-    let output = ariel_os_hal::gpio::Output::new(pin, ariel_os_embassy_common::gpio::Level::Low);
-    core::mem::forget(output);
-}
 }
