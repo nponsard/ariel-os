ffab02371e57c9e10204e92936112d2416a6fc9c
Nils Ponsard <nils.ponsard@inria.fr>
2025-11-13 12:28:22 +0100

    feat(ble): do not break the API

--- src/ariel-os-nrf/src/ble.rs
+++ src/ariel-os-nrf/src/ble.rs
@@ -21,8 +21,14 @@ use crate::{irqs::Irqs, peripheral::Peri};
 
 pub type BleStack = Stack<'static, SoftdeviceController<'static>, DefaultPacketPool>;
 
-static STACK: OnceLock<Mutex<CriticalSectionRawMutex, SameExecutorCell<BleStack>>> =
-    OnceLock::new();
+static STACK: OnceLock<
+    Mutex<
+        CriticalSectionRawMutex,
+        SameExecutorCell<Mutex<CriticalSectionRawMutex, Option<BleStack>>>,
+    >,
+> = OnceLock::new();
+static STACK_GUARD: StaticCell<BleStack> = StaticCell::new();
+
 static MPSL: StaticCell<MultiprotocolServiceLayer<'_>> = StaticCell::new();
 static SDC_MEM: StaticCell<sdc::Mem<SDC_MEM_SIZE>> = StaticCell::new();
 static RNG: StaticCell<ariel_os_random::CryptoRngSend> = StaticCell::new();
@@ -74,9 +80,24 @@ const L2CAP_RXQ: u8 = 2;
 // # Panics
 //
 // Panics when called from the wrong executor.
-pub async fn ble_stack() -> MutexGuard<'static, CriticalSectionRawMutex, SameExecutorCell<BleStack>>
-{
-    STACK.get().await.lock().await
+//
+//&'static MutexGuard<'static, CriticalSectionRawMutex, SameExecutorCell<BleStack>>
+
+pub async fn ble_stack() -> &'static BleStack {
+    STACK_GUARD.init(
+        STACK
+            .get()
+            .await
+            .lock()
+            .await
+            .get_async()
+            .await
+            .unwrap()
+            .lock()
+            .await
+            .take()
+            .unwrap(),
+    )
 }
 
 #[cfg(context = "nrf52")]
@@ -235,7 +256,10 @@ pub fn driver(p: Peripherals, spawner: Spawner, config: ariel_os_embassy_common:
     let resources = ariel_os_embassy_common::ble::get_ble_host_resources();
 
     let stack = trouble_host::new(sdc, resources).set_random_address(config.address);
-    let _ = STACK.init(Mutex::new(SameExecutorCell::new(stack, spawner)));
+    let _ = STACK.init(Mutex::new(SameExecutorCell::new(
+        Mutex::new(Some(stack)),
+        spawner,
+    )));
 
     debug!("nRF BLE driver initialized");
 }
