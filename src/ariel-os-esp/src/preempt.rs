e0a21b94dec48bf09be7746e119f24c32c85dcdd
Kaspar Schleiser <kaspar@schleiser.de>
2025-11-14 12:02:47 +0100

    fix(esp): update for esp-radio-rtos-driver
    Signed-off-by: Kaspar Schleiser <kaspar@schleiser.de>

--- src/ariel-os-esp/src/preempt.rs
+++ src/ariel-os-esp/src/preempt.rs
@@ -1,13 +1,13 @@
-//! This module provides the hooks for `esp-wifi` to schedule its threads
+//! This module provides the hooks for `esp-radio` to schedule its threads
 //! with the Ariel OS scheduler.
 
 #![expect(unsafe_code)]
 
-use core::{ffi::c_void, sync::atomic::AtomicUsize};
+use core::{ffi::c_void, ptr::NonNull, sync::atomic::AtomicUsize};
 
 use ariel_os_debug::log::{debug, trace};
-use ariel_os_threads::{THREAD_COUNT, create_raw, current_tid, yield_same};
-use esp_wifi::{TimeBase, preempt::Scheduler};
+use ariel_os_threads::{CoreAffinity, CoreId, THREAD_COUNT, create_raw, current_tid, yield_same};
+use esp_radio_rtos_driver::Scheduler;
 use esp_wifi_sys::include::malloc;
 
 /// The `AtomicUsize`s make sure this static is not promoted to read-only data.
@@ -17,16 +17,38 @@ static THREAD_SEMAPHORES: [AtomicUsize; THREAD_COUNT] =
 struct ArielScheduler {}
 
 impl Scheduler for ArielScheduler {
-    fn setup(&self, _timer: TimeBase) {
-        trace!("{}:{} setup()", file!(), line!());
-    }
-
-    fn disable(&self) {
-        trace!("{}:{} disable()", file!(), line!());
-    }
-
     fn yield_task(&self) {
         yield_same();
+    }
+
+    fn yield_task_from_isr(&self) {
+        yield_same();
+    }
+
+    fn initialized(&self) -> bool {
+        // In Ariel OS, the async executor initializing WiFi is run in a thread, so this is always
+        // true.
+        true
+    }
+
+    fn max_task_priority(&self) -> u32 {
+        const fn usize_to_u32(x: usize) -> u32 {
+            if x > u32::MAX as usize {
+                panic!("value too large for u32");
+            }
+            x as u32
+        }
+        const { usize_to_u32(ariel_os_threads::SCHED_PRIO_LEVELS - 1) }
+    }
+
+    fn usleep(&self, us: u32) {
+        todo!("not implemented");
+    }
+
+    fn now(&self) -> u64 {
+        esp_hal::time::Instant::now()
+            .duration_since_epoch()
+            .as_micros()
     }
 
     fn current_task(&self) -> *mut c_void {
@@ -37,8 +59,11 @@ impl Scheduler for ArielScheduler {
 
     fn task_create(
         &self,
+        _name: &str,
         task: extern "C" fn(*mut c_void),
         param: *mut c_void,
+        priority: u32,
+        pin_to_core: Option<u32>,
         task_stack_size: usize,
     ) -> *mut c_void {
         trace!("{}:{} task_create()", file!(), line!());
@@ -51,16 +76,22 @@ impl Scheduler for ArielScheduler {
         let stack_slice: &'static mut [u8] =
             unsafe { core::slice::from_raw_parts_mut(stack as *mut u8, task_stack_size as usize) };
 
-        let prio = ariel_os_embassy_common::executor_thread::PRIORITY;
-        let core_affinity = None;
+        let core_affinity = pin_to_core.map(|x| CoreAffinity::one(CoreId::new(x as u8)));
 
         // SAFETY: *transmuting* between any two function pointers is fine.
         let task =
             unsafe { core::mem::transmute::<extern "C" fn(*mut c_void), extern "Rust" fn()>(task) };
 
         // SAFETY: Upholding `create_raw()` invariants: We know what we are doing.
-        let tid =
-            unsafe { create_raw(task, Some(param as usize), stack_slice, prio, core_affinity) };
+        let tid = unsafe {
+            create_raw(
+                task,
+                Some(param as usize),
+                stack_slice,
+                priority as u8,
+                core_affinity,
+            )
+        };
 
         usize::from(tid) as *mut c_void
     }
@@ -75,14 +106,14 @@ impl Scheduler for ArielScheduler {
         );
     }
 
-    fn current_task_thread_semaphore(&self) -> *mut c_void {
+    fn current_task_thread_semaphore(&self) -> NonNull<()> {
         trace!("{}:{} current_task_thread_semaphore()", file!(), line!());
         // NOTE(no-panic): this is always called from within a thread, so the `unwrap()` doesn't
         // panic.
         let tid = usize::from(current_tid().unwrap());
 
-        THREAD_SEMAPHORES[tid].as_ptr() as *mut c_void
+        NonNull::new(THREAD_SEMAPHORES[tid].as_ptr() as *mut ()).unwrap()
     }
 }
 
-esp_wifi::scheduler_impl!(static SCHEDULER: ArielScheduler = ArielScheduler{});
+esp_radio_rtos_driver::scheduler_impl!(static SCHEDULER: ArielScheduler = ArielScheduler{});
