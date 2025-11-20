cb3c488b15baf3fc754bb98841dcddca91a8ddf5
Nils Ponsard <nils.ponsard@inria.fr>
2025-11-20 15:41:13 +0100

    fix(ble): remove unused ble-config-override feature
    Signed-off-by: Nils Ponsard <nils.ponsard@inria.fr>

--- src/ariel-os-embassy/src/ble.rs
+++ src/ariel-os-embassy/src/ble.rs
@@ -1,9 +1,3 @@
-#![allow(unsafe_code)]
-#![allow(
-    clippy::undocumented_unsafe_blocks,
-    reason = "should be addressed eventually"
-)]
-
 use ariel_os_embassy_common::ble::Config;
 
 // Must be async and return &trouble_host::Stack<'static, impl Controller>
@@ -11,15 +5,5 @@ pub use crate::hal::ble::ble_stack;
 
 #[allow(dead_code, reason = "false positive during builds outside of laze")]
 pub(crate) fn config() -> Config {
-    #[cfg(not(feature = "ble-config-override"))]
-    {
-        Config::default()
-    }
-    #[cfg(feature = "ble-config-override")]
-    {
-        unsafe extern "Rust" {
-            fn __ariel_os_ble_config() -> Config;
-        }
-        unsafe { __ariel_os_ble_config() }
-    }
+    Config::default()
 }
