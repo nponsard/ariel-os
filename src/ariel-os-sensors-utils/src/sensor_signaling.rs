b1a8dc0689ebfb25b41658b3b3a3ad15be336bae
Nils Ponsard <nils.ponsard@inria.fr>
2025-11-28 11:52:40 +0100

    feat: use signal

--- src/ariel-os-sensors-utils/src/sensor_signaling.rs
+++ src/ariel-os-sensors-utils/src/sensor_signaling.rs
@@ -1,11 +1,13 @@
-use ariel_os_sensors::sensor::{ReadingError, ReadingResult, ReadingWaiter, Samples};
-use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, channel::Channel};
+use ariel_os_sensors::sensor::{ReadingError, ReadingResult, ReadingWaiter, Sample, Samples};
+use embassy_sync::{
+    blocking_mutex::raw::CriticalSectionRawMutex, channel::Channel, signal::Signal,
+};
 use embassy_time::{Duration, Timer};
 use portable_atomic::{AtomicBool, Ordering};
 
 // TODO: rename this
 pub struct SensorSignaling {
-    trigger: Waiter,
+    trigger: Signal<CriticalSectionRawMutex, ()>,
     reading_channel: Channel<CriticalSectionRawMutex, ReadingResult<Samples>, 1>,
 }
 
@@ -13,7 +15,7 @@ impl SensorSignaling {
     #[must_use]
     pub const fn new() -> Self {
         Self {
-            trigger: Waiter::new(),
+            trigger: Signal::new(),
             reading_channel: Channel::new(),
         }
     }
@@ -22,7 +24,7 @@ impl SensorSignaling {
         // Remove the possibly lingering reading.
         self.reading_channel.clear();
 
-        self.trigger.signal();
+        self.trigger.signal(());
     }
 
     pub async fn wait_for_trigger(&self) {
@@ -40,36 +42,6 @@ impl SensorSignaling {
     pub fn wait_for_reading(&'static self) -> ReadingWaiter {
         ReadingWaiter::Waiter {
             waiter: self.reading_channel.receive(),
-        }
-    }
-}
-
-#[derive(Debug)]
-struct Waiter {
-    signaled: AtomicBool,
-}
-
-impl Waiter {
-    const fn new() -> Self {
-        Self {
-            signaled: AtomicBool::new(false),
-        }
-    }
-
-    fn signal(&self) {
-        self.signaled.store(true, Ordering::Release);
-    }
-
-    // FIXME: return a more efficient Future, that does not rely on Timer, but is also small
-    // memory-wise.
-    async fn wait(&self) {
-        // Wait for the Waiter to be signaled, and reset it when that happens.
-        while self
-            .signaled
-            .compare_exchange_weak(true, false, Ordering::Release, Ordering::Acquire)
-            .is_err()
-        {
-            Timer::after(Duration::from_millis(1)).await;
         }
     }
 }
