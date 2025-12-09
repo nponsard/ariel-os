87bd355ef2e596be02602c7c9c29d273b3d9768c
chrysn <chrysn@fsfe.org>
2025-12-09 02:57:51 +0100

    feat(boards): advertise UARTs of some boards
    Signed-off-by: chrysn <chrysn@fsfe.org>
    Signed-off-by: Kaspar Schleiser <kaspar@schleiser.de>
    Signed-off-by: ROMemories <152802150+ROMemories@users.noreply.github.com>

--- src/ariel-os-boards/src/nrf9151-dk.rs
+++ src/ariel-os-boards/src/nrf9151-dk.rs
@@ -8,6 +8,9 @@ pub mod pins {
         ButtonPeripherals { button0 : P0_08, button1 : P0_09, button2 : P0_18, button3 :
         P0_19, }
     );
+    ariel_os_hal::define_uarts![
+        { name : uart0, device : SERIAL3, tx : P0_27, rx : P0_26, host_facing : true },
+    ];
 }
 #[allow(unused_variables)]
 pub fn init(peripherals: &mut ariel_os_hal::hal::OptionalPeripherals) {}
