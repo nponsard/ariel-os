c178539ef6b45696bdc6f64af88c6c220cf64cf8
Antoine Lavandier <antoine.lavandier@inria.fr>
2025-12-10 17:15:17 +0100

    fix(deps): correct version range + note about buffer use
    Signed-off-by: Antoine Lavandier <antoine.lavandier@inria.fr>

--- src/ariel-os-storage/src/postcard_value.rs
+++ src/ariel-os-storage/src/postcard_value.rs
@@ -46,6 +46,8 @@ impl<'d, T: Serialize + Deserialize<'d>> Value<'d> for PostcardValue<T> {
         Ok(used.len())
     }
 
+    /// # Note
+    /// This will assume that the entire buffer is used for deserialization and thus discard it
     fn deserialize_from(buffer: &'d [u8]) -> Result<(Self, usize), SerializationError> {
         let value = from_bytes(buffer).map_err(|e| match e {
             postcard::Error::DeserializeUnexpectedEnd => SerializationError::InvalidData,
