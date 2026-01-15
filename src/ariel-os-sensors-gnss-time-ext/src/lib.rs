8a76342541f51b6be434922967c83f7fb0b87d5b
Nils Ponsard <nils.ponsard@inria.fr>
2026-01-15 11:42:52 +0100

    fixup! fixup! fixup! feat: add GNSS time extension

--- src/ariel-os-sensors-gnss-time-ext/src/lib.rs
+++ src/ariel-os-sensors-gnss-time-ext/src/lib.rs
@@ -28,6 +28,7 @@ use ariel_os_sensors::{
 };
 
 // 2025-01-01T00:00:00.000Z in nanoseconds
+// Obtained using `date --date "2025-01-01 0:00Z" -u +%s000000`
 const ARIEL_EPOCH: i64 = 1_735_689_600_000_000;
 
 /// Error returned when trying to access the time information on [`Samples`] coming from a GNSS sensor.
@@ -49,8 +50,32 @@ impl core::fmt::Display for GnssTimeExtError {
     }
 }
 
+/// Error returned when trying convert the time to two parts.
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub enum GnssTimeExtConvertError {
+    /// Timestamp is either too far in the past or too far in the future.
+    Overflow,
+}
+
+impl core::fmt::Display for GnssTimeExtConvertError {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self {
+            GnssTimeExtConvertError::Overflow => write!(f, "time conversion overflowed"),
+        }
+    }
+}
+
+// Only implement for Samples
+mod private {
+    use ariel_os_sensors::sensor::Samples;
+
+    pub trait Sealed {}
+    impl Sealed for Samples {}
+}
+
 /// Trait to use to access time information on [`Samples`] coming from a GNSS sensor.
-pub trait GnssTimeExt {
+pub trait GnssTimeExt: private::Sealed {
     /// Returns the UTC time in seconds since UNIX epoch.
     ///
     /// # Errors
@@ -100,24 +125,26 @@ impl GnssTimeExt for Samples {
 ///
 /// `utc_datetime` is an UTC timestamp as nanoseconds since UNIX epoch.
 ///
-/// # Panics
-/// Panics if the seconds since Ariel's epoch overflow or the nanoseconds in a second overflow,
-/// the first panic would happen if the timestamp is too far in the future or too far in the past.
+/// # Errors
+/// Returns an erro when `utc_datetime` is either too far in the past or too far in the future.
 ///
-/// The Ariel epoch needs to be changed before this happens.
-///
-/// For the second panic, it should never happen unless something went very wrong,
-/// as the value range should be between 1,000,000 and -1,000,000.
-#[must_use]
-pub fn convert_datetime_to_parts(utc_datetime: i64) -> (i32, i32) {
-    let nanoseconds_since_epoch = utc_datetime - ARIEL_EPOCH;
+pub fn convert_datetime_to_parts(utc_datetime: i64) -> Result<(i32, i32), GnssTimeExtConvertError> {
+    let nanoseconds_since_epoch = utc_datetime
+        .checked_sub(ARIEL_EPOCH)
+        .ok_or(GnssTimeExtConvertError::Overflow)?;
     let seconds_since_epoch = nanoseconds_since_epoch / 1_000_000;
-    let nanoseconds = nanoseconds_since_epoch - seconds_since_epoch * 1_000_000;
+    let nanoseconds = nanoseconds_since_epoch
+        .checked_sub(seconds_since_epoch * 1_000_000)
+        .ok_or(GnssTimeExtConvertError::Overflow)?;
 
-    (
-        seconds_since_epoch.try_into().unwrap(),
-        nanoseconds.try_into().unwrap(),
-    )
+    Ok((
+        seconds_since_epoch
+            .try_into()
+            .map_err(|_| GnssTimeExtConvertError::Overflow)?,
+        nanoseconds
+            .try_into()
+            .map_err(|_| GnssTimeExtConvertError::Overflow)?,
+    ))
 }
 
 fn get_element_after_marker(
