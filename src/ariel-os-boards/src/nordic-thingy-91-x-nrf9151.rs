35cfb0a7b85d79982f1bb6ba215a9586228c8bff
jkr-ariel <210673275+jkr-ariel@users.noreply.github.com>
2026-01-22 10:57:55 +0100

    feat(boards): add green and blue LED channels to `nordic-thingy-91-x`
    Signed-off-by: jkr-ariel <210673275+jkr-ariel@users.noreply.github.com>

--- src/ariel-os-boards/src/nordic-thingy-91-x-nrf9151.rs
+++ src/ariel-os-boards/src/nordic-thingy-91-x-nrf9151.rs
@@ -2,7 +2,9 @@
 
 pub mod pins {
     use ariel_os_hal::hal::peripherals;
-    ariel_os_hal::define_peripherals!(LedPeripherals { led0 : P0_29, });
+    ariel_os_hal::define_peripherals!(
+        LedPeripherals { led0 : P0_29, led1 : P0_31, led2 : P0_30, }
+    );
     ariel_os_hal::define_peripherals!(ButtonPeripherals { button0 : P0_26, });
 }
 #[allow(unused_variables)]
