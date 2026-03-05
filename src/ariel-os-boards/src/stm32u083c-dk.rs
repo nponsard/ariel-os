65569ee25534cd277a919ad40a5f29ce070352ad
Antoine Lavandier <antoine.lavandier@inria.fr>
2026-03-05 09:53:39 +0100

    fix: label the remaining user LEDs and order them according to the board
    Signed-off-by: Antoine Lavandier <antoine.lavandier@inria.fr>

--- src/ariel-os-boards/src/stm32u083c-dk.rs
+++ src/ariel-os-boards/src/stm32u083c-dk.rs
@@ -2,7 +2,9 @@
 
 pub mod pins {
     use ariel_os_hal::hal::peripherals;
-    ariel_os_hal::define_peripherals!(LedPeripherals { led0 : PA5, });
+    ariel_os_hal::define_peripherals!(
+        LedPeripherals { led0 : PC13, led1 : PA5, led2 : PB2, }
+    );
     ariel_os_hal::define_peripherals!(ButtonPeripherals { button0 : PC2, });
 }
 #[allow(unused_variables)]
