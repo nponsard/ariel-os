3edee54e97d715adf5884bcbc47ae9ac03e27ba3
Nils Ponsard <nils.ponsard@inria.fr>
2026-03-26 17:20:26 +0100

    fix(sensor-utils): return previous state
    `AtomiState::set_mode()` was returning the new state where it was
    supposed to return the previous state.
    
    Signed-off-by: Nils Ponsard <nils.ponsard@inria.fr>

--- src/ariel-os-sensors-utils/src/atomic_state.rs
+++ src/ariel-os-sensors-utils/src/atomic_state.rs
@@ -1,4 +1,4 @@
-use ariel_os_sensors::sensor::{Mode, State};
+use ariel_os_sensors::sensor::{Mode, SetModeError, State};
 use portable_atomic::{AtomicU8, Ordering};
 
 /// A helper to store [`State`] as an atomic.
@@ -34,26 +34,30 @@ impl AtomicState {
         self.state.store(state as u8, Ordering::Release);
     }
 
-    /// Sets the current mode.
-    pub fn set_mode(&self, mode: Mode) -> State {
+    /// Sets the state from a requested mode and returns the previous state.
+    ///
+    /// # Errors
+    ///
+    /// Returns [`SetModeError::Uninitialized`] if the current state is [`State::Uninitialized`].
+    #[expect(clippy::missing_panics_doc, reason = "cannot actually panic")]
+    pub fn set_mode(&self, mode: Mode) -> Result<State, SetModeError> {
         let new_state = State::from(mode);
 
         // Set the mode if the current state is not uninitialized
-        let res = self
-            .state
+        self.state
             .fetch_update(Ordering::Release, Ordering::Acquire, |s| {
                 if s == State::Uninitialized as u8 {
                     None
                 } else {
                     Some(new_state as u8)
                 }
-            });
-
-        if res.is_err() {
-            State::Uninitialized
-        } else {
-            new_state
-        }
+            })
+            .map(|s| {
+                // NOTE(no-panic): cast cannot fail because the integer value always comes from *us*
+                // internally casting `State`.
+                State::try_from(s).unwrap()
+            })
+            .map_err(|_| SetModeError::Uninitialized)
     }
 }
 
@@ -74,13 +78,33 @@ mod tests {
         state.set(State::Enabled);
         assert_eq!(state.get(), State::Enabled);
 
-        state.set_mode(Mode::Sleeping);
+        assert!(matches!(state.set_mode(Mode::Sleeping), Ok(State::Enabled)));
         assert_eq!(state.get(), State::Sleeping);
 
-        state.set_mode(Mode::Enabled);
+        assert!(matches!(state.set_mode(Mode::Enabled), Ok(State::Sleeping)));
         assert_eq!(state.get(), State::Enabled);
 
-        state.set(State::Sleeping);
+        assert!(matches!(state.set_mode(Mode::Sleeping), Ok(State::Enabled)));
         assert_eq!(state.get(), State::Sleeping);
+    }
+
+    #[test]
+    fn uninitialized_err() {
+        let state = AtomicState::new(State::Uninitialized);
+
+        assert!(matches!(
+            state.set_mode(Mode::Sleeping),
+            Err(SetModeError::Uninitialized)
+        ));
+
+        assert!(matches!(
+            state.set_mode(Mode::Enabled),
+            Err(SetModeError::Uninitialized)
+        ));
+
+        assert!(matches!(
+            state.set_mode(Mode::Disabled),
+            Err(SetModeError::Uninitialized)
+        ));
     }
 }
