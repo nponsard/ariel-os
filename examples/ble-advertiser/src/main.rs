b1d2d70e922eed920bada5424653f8495937c562
Nils Ponsard <nils.ponsard@inria.fr>
2026-06-26 15:51:49 +0200

    wip: using constants
    Signed-off-by: Nils Ponsard <nils.ponsard@inria.fr>

--- examples/ble-advertiser/src/main.rs
+++ examples/ble-advertiser/src/main.rs
@@ -5,6 +5,7 @@
 use ariel_os::{log::info, reexports::embassy_time, time::Timer};
 use embassy_futures::join::join;
 use embassy_time::Duration;
+use heapless::Vec;
 use trouble_host::advertise::{
     AdStructure, Advertisement, AdvertisementParameters, BR_EDR_NOT_SUPPORTED,
     LE_GENERAL_DISCOVERABLE,
@@ -20,24 +21,36 @@ async fn run_advertisement() {
 
     let mut adv_data = [0; 31];
 
+    const BEACON_TYPE: [u8; 2] = [0x02, 0x15];
+    const BEACON_UUID: [u8; 16] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+    const BEACON_MAJOR: [u8; 2] = [0, 0];
+    const BEACON_MINOR: [u8; 2] = [0, 0];
+    const BEACON_MEASURED_POWER: [u8; 1] = [0];
+
+    let mut manufacturer_payload: Vec<_, 27> = Vec::new();
+    manufacturer_payload
+        .extend_from_slice(&BEACON_TYPE)
+        .unwrap();
+    manufacturer_payload
+        .extend_from_slice(&BEACON_UUID)
+        .unwrap();
+    manufacturer_payload
+        .extend_from_slice(&BEACON_MAJOR)
+        .unwrap();
+    manufacturer_payload
+        .extend_from_slice(&BEACON_MINOR)
+        .unwrap();
+    manufacturer_payload
+        .extend_from_slice(&BEACON_MEASURED_POWER)
+        .unwrap();
+
     let len = AdStructure::encode_slice(
         &[
             AdStructure::Flags(LE_GENERAL_DISCOVERABLE | BR_EDR_NOT_SUPPORTED),
             AdStructure::ManufacturerSpecificData {
                 // Apple company identifier [0x4c, 0x00]
                 company_identifier: 0x4c00,
-                payload: &[
-                    // Beacon type
-                    0x02, 0x15,
-                   // Beacon data (16 bytes)
-                    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
-                    // Major
-                    0,0,
-                    // Minor
-                    0,0,
-                    // Measured power
-                    0
-                ],
+                payload: &manufacturer_payload,
             },
         ],
         &mut adv_data[..],
