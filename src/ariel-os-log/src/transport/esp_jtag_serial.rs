5151e8f1cc21106b0902d7ca1ab493ca2d5766e3
Nils Ponsard <nils.ponsard@inria.fr>
2026-07-30 16:00:55 +0200

    wip: remove oncelock in jtag-serial
    Signed-off-by: Nils Ponsard <nils.ponsard@inria.fr>

--- src/ariel-os-log/src/transport/esp_jtag_serial.rs
+++ src/ariel-os-log/src/transport/esp_jtag_serial.rs
@@ -1,27 +1,27 @@
 use core::cell::RefCell;
 
-use embassy_sync::{
-    blocking_mutex::{Mutex, raw::CriticalSectionRawMutex},
-    once_lock::OnceLock,
-};
+use embassy_sync::blocking_mutex::{Mutex, raw::CriticalSectionRawMutex};
 use esp_hal::{Blocking, usb_serial_jtag::UsbSerialJtag};
 
-static JTAG_SERIAL: OnceLock<Mutex<CriticalSectionRawMutex, RefCell<UsbSerialJtag<'_, Blocking>>>> =
-    OnceLock::new();
+// Limit the amount of locks, we don't use a OnceLock here.
+static JTAG_SERIAL: Mutex<CriticalSectionRawMutex, RefCell<Option<UsbSerialJtag<'_, Blocking>>>> =
+    Mutex::new(RefCell::new(None));
 
 pub(crate) fn write_bytes(bytes: &[u8]) {
-    if let Some(mutex) = JTAG_SERIAL.try_get() {
-        mutex.lock(|cell| {
-            cell.borrow_mut().write(bytes);
-        })
-    }
+    JTAG_SERIAL.lock(|cell| {
+        if let Some(serial) = cell.borrow_mut().as_mut() {
+            serial.write(bytes);
+        }
+    })
 }
 pub(crate) fn flush() {
-    if let Some(mutex) = JTAG_SERIAL.try_get() {
-        let _ = mutex.lock(|cell| cell.borrow_mut().flush_tx());
-    }
+    JTAG_SERIAL.lock(|cell| {
+        if let Some(serial) = cell.borrow_mut().as_mut() {
+            serial.flush_tx();
+        }
+    });
 }
 
 pub fn init(usb_device: esp_hal::peripherals::USB_DEVICE<'static>) {
-    let _ = JTAG_SERIAL.init(Mutex::new(RefCell::new(UsbSerialJtag::new(usb_device))));
+    let _ = JTAG_SERIAL.lock(|cell| cell.replace(Some(UsbSerialJtag::new(usb_device))));
 }
