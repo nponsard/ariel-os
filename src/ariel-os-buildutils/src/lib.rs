9daa092a4fdc53de6385f3b0b9e0076378672522
Antoine Lavandier <antoine.lavandier@inria.fr>
2026-09-07 15:09:09 +0200

    fix: clippy must_use_candidate lint
    Signed-off-by: Antoine Lavandier <antoine.lavandier@inria.fr>

--- src/ariel-os-buildutils/src/lib.rs
+++ src/ariel-os-buildutils/src/lib.rs
@@ -1,12 +1,14 @@
 //! Ariel OS build-time tools.
 
 /// Returns the first of the given contexts that is in the current `cfg` contexts.
+#[must_use]
 pub fn context_any(contexts: &[&'static str]) -> Option<&'static str> {
     // Contexts cannot include commas.
     contexts.iter().find(|c| context(c)).copied()
 }
 
 /// Returns whether the given context is in the current 'cfg' contexts.
+#[must_use]
 pub fn context(context: &'static str) -> bool {
     let Ok(context_var) = std::env::var("CARGO_CFG_CONTEXT") else {
         return false;
