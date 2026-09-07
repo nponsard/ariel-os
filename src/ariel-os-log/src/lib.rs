340da247d1100ee8d370c3916eb8e044d9dd667b
Nils Ponsard <nils.ponsard@inria.fr>
2026-09-07 12:53:51 +0200

    docs(log): show transport module
    Signed-off-by: Nils Ponsard <nils.ponsard@inria.fr>
    Co-authored-by: ROMemories <152802150+ROMemories@users.noreply.github.com>

--- src/ariel-os-log/src/lib.rs
+++ src/ariel-os-log/src/lib.rs
@@ -22,7 +22,10 @@
 mod _featurecomb {}
 
 #[doc(hidden)]
-#[cfg(feature = "_pluggable-transport")]
+#[cfg(feature = "internal-transport-driver")]
+pub mod transport;
+
+#[cfg(feature = "custom-transport-driver")]
 pub mod transport;
 
 #[allow(unused, reason = "conditional compilation")]
