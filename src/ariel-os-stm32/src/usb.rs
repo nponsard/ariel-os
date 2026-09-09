351ce4d904c695a9e6eae6a414c476bd65d11d6d
Nils Ponsard <nils.ponsard@inria.fr>
2026-09-09 18:38:50 +0200

    fix(docs): update broken link about embassy USB
    Signed-off-by: Nils Ponsard <nils.ponsard@inria.fr>

--- src/ariel-os-stm32/src/usb.rs
+++ src/ariel-os-stm32/src/usb.rs
@@ -97,7 +97,7 @@ pub fn driver(peripherals: Peripherals) -> UsbDriver {
     // Note: some boards don't have this wired up and might not require it,
     // as they are powered through usb!
     // If you hang on boot, try setting this to "false"!
-    // See https://embassy.dev/book/dev/faq.html#_the_usb_examples_are_not_working_on_my_board_is_there_anything_else_i_need_to_configure
+    // See https://embassy.dev/book/#_the_usb_examples_are_not_working_on_my_board_is_there_anything_else_i_need_to_configure
     // for more information
     // NOTE(board-config)
     config.vbus_detection =
