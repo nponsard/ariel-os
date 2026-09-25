8d6d517a16d6b3f767c1ae8c2018d42a0c076af5
Nils Ponsard <nils.ponsard@inria.fr>
2026-09-25 17:37:03 +0200

    fixup! feat(bootloader): update rp bootloader storage

--- src/ariel-os-rp/src/bootloader.rs
+++ src/ariel-os-rp/src/bootloader.rs
@@ -4,7 +4,6 @@ use core::cell::RefCell;
 use ariel_os_embassy_common::bootloader::{
     BootLoaderBackend, BootloaderPartitions, BootloaderStorage, FlashConfig,
 };
-use embassy_boot::BootLoaderConfig;
 use embassy_embedded_hal::flash::partition::BlockingPartition;
 use embassy_rp::{OptionalPeripherals, flash::Blocking, peripherals::FLASH};
 use embassy_sync::{
