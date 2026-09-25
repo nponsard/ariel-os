b9b43df1bd7f852741538901b09a42bb67bacd4c
Nils Ponsard <nils.ponsard@inria.fr>
2026-09-25 17:34:32 +0200

    fixup! feat(bootloader): update stm32 bootloader storage

--- src/ariel-os-stm32/src/bootloader.rs
+++ src/ariel-os-stm32/src/bootloader.rs
@@ -4,7 +4,6 @@ use core::cell::RefCell;
 use ariel_os_embassy_common::bootloader::{
     BootLoaderBackend, BootloaderPartitions, BootloaderStorage, FlashConfig,
 };
-use embassy_boot::BootLoaderConfig;
 use embassy_embedded_hal::flash::partition::BlockingPartition;
 use embassy_stm32::{OptionalPeripherals, flash::Blocking};
 use embassy_sync::{
